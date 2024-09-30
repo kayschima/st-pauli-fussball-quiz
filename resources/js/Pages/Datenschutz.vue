0d6602082729cb715b52e8b2d4f4c5aa726bbd31
Kay <kayschima@web.de>
2024-09-30 21:33:01 +0200

    change and add pages

---
+++ resources/js/Pages/Datenschutz.vue
@@ -0,0 +1,40 @@
+<script setup>
+import {Head} from '@inertiajs/vue3';
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+
+defineProps({});
+
+</script>
+
+<template>
+    <Head title="Datenschutz"/>
+    <GuestLayout>
+        <h1 class="text-2xl mb-8">Datenschutz</h1>
+        <p class="mb-2">Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht,
+            nur, weil er
+            Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz
+            ihm große Freude bereiten können.</p>
+        <p class="mb-2">Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je
+            anstrengender
+            körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p>
+        <p class="mb-2">Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die
+            Entscheidung trifft,
+            eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz
+            vermeidet, welcher keine daraus resultierende Freude nach sich zieht?</p>
+        <p class="mb-2">Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht,
+            nur, weil er
+            Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz
+            ihm große Freude bereiten können.</p>
+
+        <p class="mb-2">Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je
+            anstrengender
+            körperlicher Betätigung, außer um Vorteile daraus zu ziehen?</p>
+
+        <p class="mb-2">Aber wer hat irgend ein Recht, einen Menschen zu tadeln, der die
+            Entscheidung trifft,
+            eine Freude zu genießen, die keine unangenehmen Folgen hat, oder einen, der Schmerz
+            vermeidet, welcher keine daraus resultierende Freude nach sich zieht? Auch gibt es
+            niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur,</p>
+    </GuestLayout>
+    >
+</template>
